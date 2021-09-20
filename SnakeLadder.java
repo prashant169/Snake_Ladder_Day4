4b24f13f09868fba53aafb981b15fee7dcf276b8
prashant169 <prashantphad2018@gmail.com>
2021-09-20 04:19:03 -0700

    Player Gets To Exact Wining Position 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,48 +1,37 @@
 import java.util.Random;
-import java.util.*;
 
-public class SnakeLadder
-{
+public class SnakeLadder{
 
-	public static void main(String[] args) 
-		{
-			//UC_1
-			RollDice();
-			//UC_3
-			option();
-		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		option();
+	}
+
+	public static void option() {
+		int position = 0;
+		while (position <= 100) {
+			int luck = (int) (Math.floor(Math.random() * 10) % 3 + 1);
+			int dice = rolls_dice();
+			System.out.println("the dice is " + dice);
+			System.out.println("the luck is " + luck);
+			switch (luck) {
+			case 1:
+				System.out.println("The position is " + position);
+				break;
+			case 2:
+				position += dice;
+				System.out.println("Playing: " + position);
+				break;
+			default:
+				System.out.println("player not playing!!!");
+				break;
+			}
 		}
+	}
 
-	static int RollDice() 
-		{
-			Random rn = new Random();
-			int dice = rn.nextInt(6) + 1;
-			System.out.println("Roll_Dice: " + dice);
-		}
-
-	//UC_3
-
-	public static void option()
-		{
-    			int position=0;
-    			int luck=(int)(Math.floor(Math.random()*10)%3+1);
-    			int dice=RollDice();
-    			System.out.println("the dice is "+dice);
-    	
-			switch (luck) 
-			{
-    				case 1:System.out.println("The position is "+position);
-    				break;
-
-			    	case 2:	position+=dice;
-    				System.out.println("Playing: "+position);
-    				break;
-    
-				default:
-    					System.out.println("player not playing!!!");
-    				break;
-    			}
-    
-		}
-
+	public static int rolls_dice() {
+		int dice = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		return dice;
+	}
 }
