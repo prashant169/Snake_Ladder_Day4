bf8207face1afa0978a6ba5f8edfa86dbe6ed173
prashant169 <prashantphad2018@gmail.com>
2021-09-20 04:04:08 -0700

    Roll the Dice Using Random

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,8 +1,20 @@
+import java.util.Random;
+import java.util.*;
+
 public class SnakeLadder {
 
 	public static void main(String[] args) {
-		System.out.println("Wlcome To The Snake Ladder Game");
-		// TODO Auto-generated method stub
+		
+		// UC1_SinglePlayer
+		int position = 0;
+		
+		// UC2_Roll_Dice using Random
+		RollDice();
 	}
 
+	static void RollDice() {
+		Random rn = new Random();
+		int dice = rn.nextInt(6) + 1;
+		System.out.println("Roll_Dice: " + dice);
+	}
 }
